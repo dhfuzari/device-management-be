36b3766d638471165a937c8c17442760e4d7f85e
Daniel H. Fuzari <dhfuzari@gmail.com>
2022-04-05 18:12:19 -0300

    refactor: handle errors with try/catch and use async await

--- src/controllers/deviceController.js
+++ src/controllers/deviceController.js
@@ -1,11 +1,8 @@
-const mySql = require("../mysql").pool;
+const mySql = require("../mysql");
 
-exports.getDevices = (req, res, next) => {
-  mySql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    conn.query(
+exports.getDevices = async (req, res, next) => {
+  try {
+    const result = await mySql.execute(
       `SELECT 
         devices.id,  
         devices.color, 
@@ -13,123 +10,87 @@ exports.getDevices = (req, res, next) => {
         categories.name as category
       FROM devices
       INNER JOIN categories
-      ON devices.categories_id = categories.id;`,
-      (error, result, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({ error: error.message });
-        }
-        res.status(200).send({
-          data: result,
-        });
-      }
+      ON devices.categories_id = categories.id;`
     );
-  });
+    return res.status(200).send({
+      data: result,
+    });
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
 
-exports.getDeviceById = (req, res, next) => {
-  mySql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    conn.query(
-      "SELECT * FROM devices WHERE id = ?",
-      [req.params.deviceId],
-      (error, result, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({ error: error.message });
-        }
-        res.status(200).send({
-          data: result,
-        });
-      }
-    );
-  });
+exports.getDeviceById = async (req, res, next) => {
+  try {
+    const result = await mySql.execute("SELECT * FROM devices WHERE id = ?", [
+      req.params.deviceId,
+    ]);
+    return res.status(200).send({
+      data: result,
+    });
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
 
-exports.createDevice = (req, res, next) => {
-  mySql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    conn.query(
+exports.createDevice = async (req, res, next) => {
+  try {
+    const currentCategory = await mySql.execute(
       "SELECT * FROM categories WHERE id = ?",
-      [req.body.categories_id],
-      (error, result, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({ error: error.message });
-        }
-        if (result.length === 0) {
-          return res.status(404).send({
-            message: "Category not found",
-          });
-        }
-        conn.query(
-          "INSERT INTO devices(color, partNumber, categories_id) VALUES(?, ?, ?)",
-          [req.body.color, req.body.partNumber, req.body.categories_id],
-          (error, result, field) => {
-            conn.release();
-            if (error) {
-              return res.status(500).send({ error: error.message });
-            }
-            res.status(201).send({
-              message: "Device created",
-              data: {
-                id: result.insertId,
-              },
-            });
-          }
-        );
-      }
+      [req.body.categories_id]
     );
-  });
+    if (currentCategory.length === 0) {
+      return res.status(404).send({
+        message: "Category not found",
+      });
+    } else {
+      const query =
+        "INSERT INTO devices(color, partNumber, categories_id) VALUES(?, ?, ?)";
+      const result = await mySql.execute(query, [
+        req.body.color,
+        req.body.partNumber,
+        req.body.categories_id,
+      ]);
+      return res.status(201).send({
+        message: "Device created",
+        data: {
+          id: result.insertId,
+        },
+      });
+    }
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
 
-exports.updateDevice = (req, res, next) => {
-  mySql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    conn.query(
+exports.updateDevice = async (req, res, next) => {
+  try {
+    const result = await mySql.execute(
       "UPDATE devices SET color = ?, partNumber = ?, categories_id = ? WHERE id = ?",
       [
         req.body.color,
         req.body.partNumber,
         req.body.categories_id,
         req.params.deviceId,
-      ],
-      (error, result, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({ error: error.message });
-        }
-        res.status(202).send({
-          message: "Device successfully updated",
-        });
-      }
+      ]
     );
-  });
+    res.status(202).send({
+      message: "Device successfully updated",
+    });
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
 
-exports.removeDevice = (req, res, next) => {
-  mySql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    conn.query(
-      "DELETE FROM devices WHERE id = ?",
-      [req.params.deviceId],
-      (error, result, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({ error: error.message });
-        }
-        res.status(202).send({
-          message: "Device successfully removed",
-        });
-      }
-    );
-  });
+exports.removeDevice = async (req, res, next) => {
+  try {
+    const result = await mySql.execute("DELETE FROM devices WHERE id = ?", [
+      req.params.deviceId,
+    ]);
+    res.status(202).send({
+      message: "Device successfully removed",
+    });
+  } catch (error) {
+    return res.status(500).send({ error });
+  }
 };
