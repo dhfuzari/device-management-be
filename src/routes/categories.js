57d32facafc1775e91532437964a4496c6f94657
Daniel H. Fuzari <dhfuzari@gmail.com>
2022-04-05 00:07:17 -0300

    refactor: categories controller created

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,115 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const mySql = require("../mysql").pool;
 
-router.get("/", (req, res, next) => {
-  mySql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    conn.query("SELECT * FROM categories", (error, result, field) => {
-      conn.release();
-      if (error) {
-        return res.status(500).send({ error: error.message });
-      }
-      res.status(200).send({
-        data: result,
-      });
-    });
-  });
-});
+const categoriesController = require("../controllers/categoriesController");
 
-router.get("/:categoryId", (req, res, next) => {
-  mySql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    conn.query(
-      "SELECT * FROM categories WHERE id = ?",
-      [req.params.categoryId],
-      (error, result, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({ error: error.message });
-        }
-        res.status(200).send({
-          data: result,
-        });
-      }
-    );
-  });
-});
-
-router.post("/", (req, res, next) => {
-  const newDevice = {
-    categoryId: req.body.categoryId,
-    color: req.body.color,
-    partNumber: req.body.partNumber,
-  };
-
-  mySql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    conn.query(
-      "INSERT INTO categories(name) VALUES(?)",
-      [req.body.name],
-      (error, result, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({ error: error.message });
-        }
-        res.status(201).send({
-          message: "Category created",
-          data: {
-            id: result.insertId,
-          },
-        });
-      }
-    );
-  });
-});
-
-router.patch("/:categoryId", (req, res, next) => {
-  mySql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    conn.query(
-      "UPDATE categories SET name = ? WHERE id = ?",
-      [req.body.name, req.params.categoryId],
-      (error, result, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({ error: error.message });
-        }
-        res.status(202).send({
-          message: "Category successfully updated",
-        });
-      }
-    );
-  });
-});
-
-router.delete("/:categoryId", (req, res, next) => {
-  mySql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error.message });
-    }
-    conn.query(
-      "DELETE FROM categories WHERE id = ?",
-      [req.params.categoryId],
-      (error, result, field) => {
-        conn.release();
-        if (error) {
-          return res.status(500).send({ error: error.message });
-        }
-        res.status(202).send({
-          message: "Category successfully removed",
-        });
-      }
-    );
-  });
-});
+router.get("/", categoriesController.getCategories);
+router.get("/:categoryId", categoriesController.getCategoriesById);
+router.post("/", categoriesController.createCategory);
+router.patch("/:categoryId", categoriesController.updateCategory);
+router.delete("/:categoryId", categoriesController.removeCategory);
 
 module.exports = router;
--- src/routes/devices.js
+++ src/routes/devices.js
@@ -3,13 +3,9 @@ const router = express.Router();
 const deviceController = require("../controllers/deviceController");
 
 router.get("/", deviceController.getDevices);
-
 router.get("/:deviceId", deviceController.getDeviceById);
-
 router.post("/", deviceController.createDevice);
-
 router.patch("/:deviceId", deviceController.updateDevice);
-
 router.delete("/:deviceId", deviceController.removeDevice);
 
 module.exports = router;
